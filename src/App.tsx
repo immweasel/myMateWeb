27afcc79978e19d472df3efc19d16ffb495b6083
darishhhhka <tarusovadarina6@gmail.com>
2024-10-05 22:16:34 +0300

    Balance

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import Auth from "./components/Auth/Auth";
 import Profile from "./components/Profile/Profile";
-import BalanceCard from "./components/BalanceCard/BalanceCard";
 import profileImg from "./assets/images/profileImg.png";
+import Balance from "./components/Balance/Balance";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       {/* <div className="App">
         <Auth />
       </div> */}
-      <Profile
+      {/* <Profile
        userImg={profileImg}
        userName="Маркова Анастасия"
        userCity="Москва"
@@ -20,9 +20,9 @@ function App() {
        cntAds="0"
         cntMoney="255"
         cntFavourites="12"
-      />
-      {/* <BalanceCard
-      money={255}/> */}
+      /> */}
+      <Balance
+      money={255}/>
     </>
 
 
---
+++ src/components/Balance/Balance.jsx
@@ -0,0 +1,17 @@
+import "./Balance.css"
+import leftArrow from "../../assets/icons/leftArrow.svg"
+import BalanceCard from "../BalanceCard/BalanceCard"
+import BalanceImg from "../../assets/images/people.png"
+
+export default ({money}) => {
+    return <div className="balance">
+        <a href="#" className="balance__arrow">
+            <img src={leftArrow} alt=""/>
+        </a>
+        <BalanceCard
+        money={money}/>
+        <div className="balance__img">
+            <img src={BalanceImg} alt="" />
+        </div>
+    </div>
+}
